f2c94d6abd18e5eb9d07a8ebdfe6dab0d40313fa
apetranik <apetranik16@punahou.edu>
2014-02-03 14:25:13 -1000

    Began work with Swing!
    If you run RateView, a window pops up that asks a question!

---
+++ RateQuestions.java
@@ -0,0 +1,15 @@
+package RateMyClass;
+import java.util.Scanner;
+
+public class RateQuestions {
+  public static String questionOne = "On a scale of 1-10, how hard was/is the course overall?";
+  public static String questionTwo = "On average, how much homework did/do you have for this course?";
+  public static String questionThree = "On a scale of 1-10, how engaged did/do you feel in this course?";
+  public static String questionFour = "On a scale of 1-10, how much did you enjoy the class?";
+  public static String questionFive = "Would you recommend this course to someone else?";
+  
+  public static String question() {
+    return questionOne;
+    
+  }
+}
--- RateView.java
+++ RateView.java
@@ -1,6 +1,33 @@
-import java.util.Scanner;
+package RateMyClass;
+import javax.swing.*;   
 
 public class RateView {
-  public RateView() {
+  static String questionOne = RateQuestions.question();
+  
+  // Create the GUI and show it. 
+  
+  private static void createAndShowGUI() {
+    
+    //Create and set up the window.
+    JFrame frame = new JFrame("Rate My Class");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    //Add the "Hello World" label.
+    JLabel label = new JLabel(RateQuestions.question());
+    frame.getContentPane().add(label);
+    
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
+  
+  public static void main(String[] args) {
+    //Schedule a job for the event-dispatching thread:
+    //creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        createAndShowGUI();
+      }
+    });
   }
 }
