a430ee4b521fa59f26341472c49d0dcbb0111e69
apetranik <apetranik16@punahou.edu>
2014-02-12 14:29:44 -1000

    Nested If Statements, More Subject Selection, Margins
    Figured out how to nest if statements by using a boolean. Added to the
    subject and course selection menu. Window is now cleared every time you
    switch subjects. I also set a margin for the window

--- RateDriver.java
+++ RateDriver.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class RateDriver {
   // lots of instance variables - mostly subjects and courses
   public static Scanner in = new Scanner(System.in);
-  public static String courseQuestion = "Which Course area do you want? Math, Sciece, English, Social Studies, Art, or P.E.?";
+  public static String courseQuestion = "Which Course area do you want? \n\nMath,  Science,  English,  Language, Social Studies,  Art, or P.E.?\n";
   public static String questionOne = "On a scale of 1-10, how hard was/is the course overall?";
   public static String questionTwo = "On average, how much homework did/do you have for this course?";
   public static String questionThree = "On a scale of 1-10, how engaged did/do you feel in this course?";
   public static String questionFour = "On a scale of 1-10, how much did you enjoy the class?";
   public static String questionFive = "Would you recommend this course to someone else?";
   public static String courseSelection;
-  public static String mathCourses = "\n\n1. Alg 1 \n2. Geometry \n3. Geometry H. \n4. Alg 2 \n5. Alg 2 H. \n6. Pre-Calc \n7. Adv. Pre-Calc \n8. Adv. Pre-Calc H. \n9. AP Calc AB \n10. AP Calc BC \n11. Diff. Calc";
-  public static String scienceCourses = "\n\n1. Biology\n2. Biology H.\n3. AP Bio\n4. Chemistry\n5. Chemistry H.\n6. AP Chem\n7. Physics\n8. Physics H.\n9. AP Physics 1 & 2\n10. AP Physics C";
-  public static String englishCourses = "\n\n1. English 1AB\n2. English 2AB\n3. American Lit";
-  public static String socialCourses = "\n\n1. ISS\n2. Contemp. Issues\n3. World Civ.\n4. Hawaiian Culture\5. Medieval History\n6. Asian History\n7. US History\n8. AP US History\n9. European History\n10. AP European History"; 
-  public static String language1 = "\n\n1. Chinese\n2. Japanese\n3. French\n4. Spanish\n5. Hawaiian";
-  public static String languageChinese = "\n\n1. Mandarin 1\n2. Mandarin 2\n3. Mandarin 2 H.\n4. Mandarin 3\n5. Mandarin 3 H.\n6. Mandarin 4\n7. Mandarin 4 H.\n8. Mandarin 5\n9. Mandarin 5 H.";
+  public static String mathCourses = "\nAlg 1 \nGeometry \nGeometry H. \nAlg 2 \nAlg 2 H. \nPre-Calc \nAdv. Pre-Calc \nAdv. Pre-Calc H. \nAP Calc AB \nAP Calc BC \nDiff. Calc";
+  public static String scienceCourses = "\nBiology\nBiology H.\nAP Bio\nChemistry\nChemistry H.\nAP Chem\nPhysics\nPhysics H.\nAP Physics 1 & 2\nAP Physics C";
+  public static String englishCourses = "\nEnglish 1AB\nEnglish 2AB\nAmerican Lit";
+  public static String socialCourses = "\nISS\nContemp. Issues\nWorld Civ.\nHawaiian Culture\nMedieval History\nAsian History\nUS History\nAP US History\nEuropean History\nAP European History"; 
+  public static String language1 = "\nChinese\nJapanese\nFrench\nSpanish\nHawaiian";
+  public static String languageChinese = "\nMandarin 1\nMandarin 2\nMandarin 2 H.\nMandarin 3\nMandarin 3 H.\nMandarin 4\nMandarin 4 H.\nMandarin 5\nMandarin 5 H.";
+  public static String languageJapanese = "\nJapanese 1\nJapanese 2\nJapanese 2 H.\nJapanese 3\nJapanese 3 H.\nJapanese 4\nJapanese 4 H.\nJapanese 5\nJapanese 5 H.";
   
   // runs the RateView Class
   public static void main(String[] args) {
--- RateView.java
+++ RateView.java
@@ -2,26 +2,33 @@ package RateMyClass;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class RateView extends JPanel implements ActionListener {
-  public static String courseQuestion = "\n Which Course area do you want? Math, Sciece, English, Social Studies, Art, or P.E.?";
+  public static String courseQuestion = RateDriver.courseQuestion;
   public static String text; // used to save the text that the user has entered
   protected JTextField textField; // controls the text field - where user types text - is editable
   protected JTextArea textArea; // controls the text Area - where text is displayed
+  Boolean language = false; //controls subset language
+
   
   public RateView() {
     super(new GridBagLayout());
     
-    textArea = new JTextArea(courseQuestion, 40, 80); // adds a new text area w/ dimensions
+    textArea = new JTextArea(courseQuestion, 20, 80); // adds a new text area w/ dimensions -- args = # rows
     
     textArea.setEditable(false); // doesn't allow the text area to be editable by the user. It just displays text
     
     JScrollPane scrollPane = new JScrollPane(textArea); // allows the text area to be scrolled through. 
     
-    textField = new JTextField(80); // adds new text field - where users can type
-    
+    textField = new JTextField(80); // adds new text field - where users can type -- arg = # of columns
     textField.addActionListener(this); //adds action listener, which allows me to know what the user clicks on or types
+    textArea.setMargin(new Insets(15,20,20,20)); //sets margin
     
+    // font
+    Font font = new Font("Century Gothic", Font.BOLD,  16);
+    textArea.setFont(font);
+    textArea.setLineWrap(true); //wraps lines if the window is too small
     
     
     GridBagConstraints gridcon = new GridBagConstraints(); // the window w/ the same dimensions that were specified above 
@@ -29,7 +36,7 @@ public class RateView extends JPanel implements ActionListener {
     gridcon.fill = GridBagConstraints.BOTH;    
     gridcon.weightx = 1.0; // allows users to pull the constraints and change the size of the window
     gridcon.weighty = 1.0;
-   
+    
     add(scrollPane, gridcon); // allows text area to be displayed
     add(textField, gridcon); // allows text field to be displayed
     
@@ -46,28 +53,44 @@ public class RateView extends JPanel implements ActionListener {
     
     // controls what will be displayed for each subject
     if (newText.equals("math")) {
+      textArea.setText("\nMATH\n-----------------------------\n");
       textArea.append(RateDriver.mathCourses);
     }
     else if (newText.equals("science")) {
-
+      textArea.setText("\nSCIENCE\n-----------------------------\n");
       textArea.append(RateDriver.scienceCourses);
     }
     else if (newText.equals("english")) {
+      textArea.setText("\nENGLISH\n-----------------------------\n");
       textArea.append(RateDriver.englishCourses);
     }
     else if (newText.equals("social studies")) {
+      textArea.setText("\nSOCIAL STUDIES\n-----------------------------\n");
       textArea.append(RateDriver.socialCourses);
     }
-    else if (newText.equals("language")) {   
-      textArea.append(RateDriver.language1);
-      if (textField.equals("chinese")) {
-        textArea.append(RateDriver.languageChinese);
-      }
+    else if (newText.equals("language")) {
+      textArea.setText("\nLANGUAGE\n-----------------------------\n");
+      language = true;
+      textArea.append(RateDriver.language1); 
     }
+    
+    else if (newText.equals("chinese") && language == true) {
+      textArea.setText("\nCHINESE\n-----------------------------\n");
+      textArea.append(RateDriver.languageChinese);   
+      language = false;
+    }
+    else if (newText.equals("japanese") && language == true) {
+      textArea.setText("\nJAPANESE\n-----------------------------\n");
+      textArea.append(RateDriver.languageJapanese);
+    }
+    
     else if (newText.equals("art")) {
+      textArea.setText("\nART\n-----------------------------\n");
       textArea.append(RateDriver.courseSelection);
+      
     }
     else if (newText.equals("pe")) {
+      textArea.setText("");
       textArea.append(RateDriver.courseSelection);
     }
     
@@ -75,13 +98,14 @@ public class RateView extends JPanel implements ActionListener {
   
   private static void createAndShowGUI() { // creates the GUI
     
-
+    
     JFrame frame = new JFrame("Rate My Class"); // creates a window and labels it Rate My Class
     // allows all the windows and stuff above to be seen
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.add(new RateView());
     frame.pack();
     frame.setVisible(true);
+
   }
   
   public static void main(String[] args) { // just displays the GUI like in grid world how it has to say "Show grid"
